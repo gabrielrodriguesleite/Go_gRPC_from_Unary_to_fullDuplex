5b8b063f943a5d0dd53ec4b57c1e16297d781286
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-07-31 20:53:10 -0300

    [feat] simples server implementado

--- server/server.go
+++ server/server.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+	pb "go-grpc/pb"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedDataServiceServer
+}
+
+func main() {
+	fmt.Println("Startin server...")
+
+	l, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	pb.RegisterDataServiceServer(s, &server{})
+	if err := s.Serve(l); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
