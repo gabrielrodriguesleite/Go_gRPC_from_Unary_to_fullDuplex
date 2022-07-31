3a72791d1fcb63b747222fa9b25caf48fe6276ce
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-07-31 20:59:31 -0300

    [feat] simples cliente implementado

--- client/client.go
+++ client/client.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	pb "go-grpc/pb"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	fmt.Println("Starting Client...")
+
+	cc, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := pb.NewDataServiceClient(cc)
+	fmt.Println(c)
+}
