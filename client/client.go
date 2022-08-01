d75297ad36b71ad6462aaaf99be632a567103af7
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-07-31 22:01:52 -0300

    [feat] server streaming cliente implementado

--- client/client.go
+++ client/client.go
@@ -4,27 +4,61 @@ import (
 	"context"
 	"fmt"
 	pb "go-grpc/pb"
+	"io"
 	"log"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-func doUnary(c pb.HeroesServiceClient) { // add for Unary
-	log.Println("Starting Unary RPC...")
+// ----- Unary -----
+// func doUnary(c pb.HeroesServiceClient) {
+// 	log.Println("Starting Unary RPC...")
 
-	req := &pb.CallRequest{
+// 	req := &pb.CallRequest{
+// 		Calling: &pb.Calling{
+// 			Hero: "Spiderman",
+// 		},
+// 	}
+
+// 	res, err := c.Call(context.Background(), req)
+// 	if err != nil {
+// 		log.Fatalf("Error Call RPC: %v", err)
+// 	}
+
+// 	log.Printf("Response from Call: %v\n", res.Result)
+// }
+
+// ----- Server Streaming -----
+func doServerStreaming(c pb.HeroesServiceClient) {
+	log.Printf("Starting Server Straming RPC...")
+
+	req := &pb.CallTeamRequest{
 		Calling: &pb.Calling{
-			Hero: "Spiderman",
+			Hero: "X-Men",
 		},
 	}
 
-	res, err := c.Call(context.Background(), req)
+	stream, err := c.CallTeam(context.Background(), req)
+
 	if err != nil {
-		log.Fatalf("Error Call RPC: %v", err)
+		log.Fatalf("Error calling Server Streaming CallTeam RPC: %v", err)
 	}
 
-	log.Printf("Response from Call: %v\n", res.Result)
+	for { // while true ?
+		res, err := stream.Recv()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Printf("Error while reading stream: %v", err)
+		}
+
+		log.Printf("Response from CallTeam: %v\n", res.GetResult())
+	}
+
 }
 
 // ----- MAIN -----
@@ -41,5 +75,8 @@ func main() {
 	// fmt.Println(c) // for previous test of server/client connection
 
 	// ----- Unary -----
-	doUnary(c)
+	// doUnary(c)
+
+	// ----- Server Streaming -----
+	doServerStreaming(c)
 }
